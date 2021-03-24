9e5344eb277bd06009200e4a3e9575ade2b0538d
James Galantino <james_galantino@aol.com>
2021-03-24 19:36:17 -0400

    Fixed typo

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
 import Header from "./components/Header/Header";
 import { StoreProvider } from "./utils/GlobalState";
 import Navbar from "./components/Navbar/Navbar";

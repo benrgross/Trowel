e6281ea8e04e53227495c8fd1957a47e7bcd3eb5
Loreto Delgado III <loretodelgadoiii@Loretos-MacBook-Pro.local>
2021-03-24 11:18:11 -0500

    added nav route to home/accounts page.

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,1 +1,14 @@
 import "./home.css"
+
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to the Home Page!</h1>
+            <h2>Accounts: </h2>
+        </div>
+    )
+}
+
+export default Home

8a53e6725ab3b237e2d2d4c33e38867a320adb7d
Loreto Delgado III <loretodelgadoiii@Loretos-MacBook-Pro.local>
2021-03-25 12:53:16 -0500

    added plant object for plant post request. added save account method to front end api.js

--- client/src/components/Results/Results.js
+++ client/src/components/Results/Results.js
@@ -1,17 +1,51 @@
 import React from "react";
 import { useStoreContext } from "../../utils/GlobalState";
 import API from "../../utils/API";
+import axios from "axios"
+
 function Results() {
   const [state, dispatch] = useStoreContext();
 
   const getPlant = async (plant) => {
-    console.log(plant);
     const item = {
       url: plant.links.self,
     };
-    console.log(item.url);
+    
     const { data } = await API.getPlant(item);
+    
+    console.log(item.url);
+    console.log(plant);
     console.log(data);
+
+    const plantObject = {
+      atmosHumidity: data.atmosHumidity,
+      bloomMonths: data.bloomMonths,
+      commonName: data.commonName,
+      edible: data.edible,
+      family: data.family,
+      familyCommonName: data.familyCommonName,
+      color: data.flowerColor.color,
+      conspicuous: data.flowerColor.conspicuous,
+      genus: data.genus,
+      growthHabit: data.growthHabit,
+      heightAvgCm: data.heightAvg.cm + "cm",
+      img: plant.img,
+      light: data.light,
+      maxPh: data.maxPh,
+      maxPrecipitation: data.maxPrecipitation.mm + "mm",
+      maxDeg_f: data.maxTemp.deg_f,
+      maxDeg_c: data.maxTemp.deg_c,
+      minPh: data.minPh,
+      minPrecipitation: data.minPrecipitation.mm + "mm",
+      minDeg_f: data.minTemp.deg_f,
+      minDeg_c: data.minTemp.deg_c,
+      native: data.native,
+      scientificName: data.scientific_name,
+      soilNutriments: data.soilNutriments,
+      soilTexture: data.soilTexture
+    };
+
+    // API.savePlant(plantObject)
 
     // Use a dispatch to send this object and set the values to viewPlant state value
     dispatch({
@@ -23,11 +57,6 @@ function Results() {
       }
     })
 
-    console.log({
-      commonName: plant.commonName,
-      scientificName: plant.scientificName,
-      img: plant.img,
-    });
   };
 
   return (

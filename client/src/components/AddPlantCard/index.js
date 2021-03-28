d0d98a35bad3c4865a3bd298cf176542ec1b757c
Ben Gross <benrgross@gmail.com>
2021-03-28 16:45:08 -0400

    plant card style

--- client/src/components/AddPlantCard/index.js
+++ client/src/components/AddPlantCard/index.js
@@ -4,7 +4,7 @@ import API from "../../utils/API";
 import { useHistory, Link } from "react-router-dom";
 import { SET_SAVED_ACCOUNT } from "../../utils/actions";
 import { FaArrowCircleLeft, FaPlus } from "react-icons/fa";
-
+import "./spotlight.css";
 function AddPlantCard() {
   const [state, dispatch] = useStoreContext();
   const {
@@ -94,65 +94,111 @@ function AddPlantCard() {
 
   return (
     <div>
-      <h1>View A Plant Here!</h1>
-      <h2>Plant Card:</h2>
+      <div className="row ">
+        <div className="col-sm-12 col-md-12 col-lg-12 d-flex justify-content-center">
+          <h1>View A Plant Here!</h1>
+        </div>
+      </div>
+
       {state.switch ? (
         <div className="container spotlight-card">
-          <button onClick={savePlantSelection}>Add Plant</button>
-          <p>Name: {commonName}</p>
-          <p>Scientific Name: {scientificName}</p>
-          {atmosHumidity ? <p>Humidity: {atmosHumidity}</p> : ""}
-          {edible ? <p>Edible: {edible}</p> : ""}
-          {family ? <p>Family: {family}</p> : ""}
-          {familyCommonName ? (
-            <p>Family Common Name: {familyCommonName}</p>
-          ) : (
-            ""
-          )}
-          {genus ? <p>Genus: {genus}</p> : ""}
-          {growthHabit ? <p>Growth Habit: {growthHabit}</p> : ""}
-          {heightAvgCm ? <p>Average Height: {heightAvgCm}</p> : ""}
-          {light ? <p>Light Level: {light}</p> : ""}
-          {maxPh ? <p>Max pH Level: {maxPh}</p> : ""}
-          {minPh ? <p>Minimum pH Level: {minPh}</p> : ""}
-          {maxPrecipitation ? <p>Max Precipitation: {maxPrecipitation}</p> : ""}
-          {minPrecipitation ? (
-            <p>Minimum Precipitation: {minPrecipitation}</p>
-          ) : (
-            ""
-          )}
-          {native ? <p>Native: {native.join(", ")}</p> : ""}
-          {soilNutriments ? <p>Soil Nutriments: {soilNutriments}</p> : ""}
-          {soilTexture ? <p>Soil Texture: {soilTexture}</p> : ""}
-          <p>Select The Light Conditions:</p>
-          <select
-            ref={lightRef}
-            style={{ width: "25%" }}
-            className="form-control"
-          >
-            <option>Full Sun</option>
-            <option>Partial Sun</option>
-            <option>Shade</option>
-            <option>Deep Shade</option>
-          </select>
-          <div className="container">
-            {img ? (
-              <img
-                className="img-thumbnail"
-                style={{ height: "200px", cursor: "pointer" }}
-                src={img}
-                alt={commonName}
-              />
-            ) : (
-              <img
-                className="img-thumbnail"
-                style={{ height: "200px", cursor: "pointer" }}
-                alt={commonName}
-                src={
-                  "https://www.creativefabrica.com/wp-content/uploads/2019/12/09/Plants-Monochrome-Icon-Vector-Graphics-1-5-580x386.jpg"
-                }
-              />
-            )}
+          <div className="row">
+            <div className="col-sm-12 col-md-12 col-lg-12">
+              <div className=" plant-view-card">
+                <div className="row card-top d-flex justify-content-center">
+                  <div className="col-sm-12 col-md-6 col-lg-6">
+                    {img ? (
+                      <img
+                        className="card-img-top img-thumbnail "
+                        style={{ height: "200px", cursor: "pointer" }}
+                        src={img}
+                        alt={commonName}
+                      />
+                    ) : (
+                      <img
+                        className="img-thumbnail"
+                        style={{ height: "200px", cursor: "pointer" }}
+                        alt={commonName}
+                        src={
+                          "https://www.creativefabrica.com/wp-content/uploads/2019/12/09/Plants-Monochrome-Icon-Vector-Graphics-1-5-580x386.jpg"
+                        }
+                      />
+                    )}
+                  </div>
+
+                  <div className="col-sm-12 col-md-6 col-lg-6">
+                    <p>Name - {commonName}</p>
+                    <p>Scientific Name - {scientificName}</p>
+
+                    {edible ? <p>Edible - {edible}</p> : ""}
+                    {family ? <p>Family - {family}</p> : ""}
+                    {familyCommonName ? (
+                      <p>Family Common Name - {familyCommonName}</p>
+                    ) : (
+                      ""
+                    )}
+                    {genus ? <p>Genus: {genus}</p> : ""}
+                  </div>
+                  <div className="hr "></div>
+                </div>
+                <div className="row">
+                  <div className="col-sm-12 col-md-6 col-lg-6 card-body info">
+                    {growthHabit ? <p>Growth Habit - {growthHabit}</p> : ""}
+                    {heightAvgCm ? <p>Average Height - {heightAvgCm}</p> : ""}
+                    {light ? <p>Light Index - {light}</p> : ""}
+                    {native ? <p>Native To - {native.join(", ")}</p> : ""}
+                  </div>
+                  <div className="col-sm-12 col-md-6 col-lg-6c card-body info">
+                    {maxPh ? <p>Max pH - {maxPh}</p> : ""}
+                    {minPh ? <p>Minimum pH - {minPh}</p> : ""}
+                    {maxPrecipitation ? (
+                      <p>Max Precipitation - {maxPrecipitation}</p>
+                    ) : (
+                      ""
+                    )}
+                    {minPrecipitation ? (
+                      <p>Minimum Precipitation - {minPrecipitation}</p>
+                    ) : (
+                      ""
+                    )}
+                    {atmosHumidity ? <p>Humidity - {atmosHumidity}</p> : ""}
+                    {soilNutriments ? (
+                      <p>Soil Nutriments - {soilNutriments}</p>
+                    ) : (
+                      ""
+                    )}
+                    {soilTexture ? <p>Soil Texture - {soilTexture}</p> : ""}
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-sm-12 col-md-6 col-lg-6 card-body info">
+                    <form>
+                      <span>How Much Sun Will It Get </span>
+                      <select
+                        ref={lightRef}
+                        className="form-control light-choose"
+                      >
+                        <option>Full Sun</option>
+                        <option>Partial Sun</option>
+                        <option>Shade</option>
+                        <option>Deep Shade</option>
+                      </select>
+                    </form>
+                  </div>
+                  <div className="col-sm-12 col-md-6 col-lg-6 d-flex card-body info add-btn">
+                    <div className="container p-2  d-flex justify-content-center">
+                      <button
+                        style={{ padding: "50px", height: "40px" }}
+                        className="btn btn-success add-plant p-2"
+                        onClick={savePlantSelection}
+                      >
+                        Add Plant
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       ) : (
@@ -168,8 +214,8 @@ function AddPlantCard() {
               onChange={(e) => changeNote(e.target.value)}
               style={style}
             ></textarea>
-            <button onClick={() => addNote(id)}>Add</button>
           </div>
+          <button onClick={() => addNote(id)}>Add</button>
         </div>
       )}
     </div>

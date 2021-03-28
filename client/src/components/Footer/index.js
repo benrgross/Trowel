62670067b602267830c8c2503e0efd85f836b453
James Galantino <james_galantino@aol.com>
2021-03-28 17:29:23 -0400

    Styled footer buttons

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { useHistory, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./style.css";
 
 import { FaArrowCircleLeft, FaPlus } from "react-icons/fa";
 
@@ -7,12 +8,12 @@ function Footer() {
   return (
     <footer>
       <Link to="/account">
-        <button className="btn btn-success" style={{ cursor: "pointer" }}>
+        <button className="btn page" style={{ cursor: "pointer" }}>
           <FaArrowCircleLeft /> Account
         </button>
       </Link>
       <Link to="/search">
-        <button className="btn btn-success" style={{ cursor: "pointer" }}>
+        <button className="btn page" style={{ cursor: "pointer" }}>
           <FaPlus /> Search Plants
         </button>
       </Link>

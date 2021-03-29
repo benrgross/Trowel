58b500cfa1c79ba9d25c9c7167420993cb52f063
Ben Gross <benrgross@gmail.com>
2021-03-28 21:37:16 -0400

    bugginfixin

--- client/src/pages/Plant/index.js
+++ client/src/pages/Plant/index.js
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
 import React, { useEffect } from "react";
 import { useStoreContext } from "../../utils/GlobalState";
 import API from "../../utils/API";
-// import { useHistory, Link } from "react-router-dom";
-import { SET_SAVED_ACCOUNT } from "../../utils/actions";
-// import { FaArrowCircleLeft, FaPlus } from "react-icons/fa";
-=======
-import React from "react";
->>>>>>> main
 import AddPlantCard from "../../components/AddPlantCard";
 
 const Plant = () => {
-<<<<<<< HEAD
   const [state, dispatch] = useStoreContext();
   useEffect(() => {
     getPlant();
@@ -70,8 +62,6 @@ const Plant = () => {
     });
   };
 
-=======
->>>>>>> main
   return (
     <div>
       <AddPlantCard />

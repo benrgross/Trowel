2fe4ed68e23292db7cf567e56175ebb1e01f981b
Ben Gross <benrgross@gmail.com>
2021-03-30 10:05:04 -0400

    pull and push

--- client/src/components/AddPlantCard/index.js
+++ client/src/components/AddPlantCard/index.js
@@ -113,7 +113,7 @@ function AddPlantCard() {
                     )}
                   </div>
 
-                  <div className="col-sm-12 col-md-6 col-lg-6">
+                  <div className="col-sm-12 col-md-5 col-lg-5">
                     <p style={{ marginTop: "20px" }}>Name - {commonName}</p>
                     <p>Scientific Name - {scientificName}</p>
 

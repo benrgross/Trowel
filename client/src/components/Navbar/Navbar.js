73044064433b137908b5983bb0c67450baf3ab0e
James Galantino <james_galantino@aol.com>
2021-03-29 21:58:13 -0400

    Styled login/search navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -14,7 +14,7 @@ function Navbar() {
   return (
     <Nav
       style={{ backgroundColor: "rgb(206 153 4 / 30%)", height: "" }}
-      className="navbar navbar-expand-md  shadow navbar-dark pr-lg-1 pl-sm-5 justify-content-between nav-con"
+      className="navbar navbar-expand-md shadow navbar-dark pr-lg-1 pl-sm-5 justify-content-between nav-con"
     >
       <Link className="navbar-brand nav-text" to="/">
         <img

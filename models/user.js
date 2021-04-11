64c6b086eca3351b210b423ddf511e9e9a8a87e8
Ben Gross <benrgross@gmail.com>
2021-04-11 10:08:43 -0400

    hash password

--- models/user.js
+++ models/user.js
@@ -1,6 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const uniqueValidator = require("mongoose-unique-validator");
 const bcrypt = require("bcryptjs");
+SALT_WORK_FACTOR = 10;
+
+// var hash_password = function( password ) {
+//   let salt = bcrypt.genSaltSync(); // enter number of rounds, default: 10
+//   let hash = bcrypt.hashSync( password, salt );
+//   return hash;
+// },
 
 const userSchema = new Schema({
   email: {
@@ -22,14 +30,21 @@ const userSchema = new Schema({
   ],
 });
 
-// userSchema.methods = {
-//   checkPassword: function (inputPassword) {
-//     return bcrypt.compareSync(inputPassword, this.local.password);
-//   },
-//   hashPassword: (plainTextPassword) => {
-//     return bcrypt.hashSync(plainTextPassword, 10);
-//   },
-// };
+userSchema.pre("save", async function (next) {
+  const user = this;
+  const hash = await bcrypt.hash(this.password, 10);
+
+  this.password = hash;
+  next();
+});
+
+userSchema.methods.isValidPassword = async function (password) {
+  const user = this;
+  const compare = await bcrypt.compare(password, user.password);
+
+  return compare;
+};
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,36 +3,58 @@ const db = require("../../models");
 const JWT = require("jsonwebtoken");
 const passport = require("../../authentication/passport");
 
-router.post("/login", (req, res, next) => {
-  console.log(req.body);
-  passport.authenticate("local", { session: false }, (err, user, info) => {
-    console.log(info);
-    console.log(err);
-    console.log(user);
-    if (err || !user) {
-      console.log(user);
-      return res.status(400).json({
-        message: "didnt work ",
+// router.post("/login", (req, res, next) => {
+//   console.log(req.body);
+//   passport.authenticate("local", { session: false }, (err, user, info) => {
+//     console.log(info);
+//     console.log(err);
+//     console.log(user);
+//     if (err || !user) {
+//       console.log(user);
+//       return res.status(400).json({
+//         message: "didnt work ",
+//       });
+//     }
+//     req.login(user, { session: false }, (err) => {
+//       if (err) res.send(err);
+//       const token = JWT.sign(user.toJSON(), process.env.PASSPORT_SECRET);
+//       res.json({ email: user.email, token: token });
+//     });
+//   })(req, res, next);
+// });
+
+router.post("/login", async (req, res, next) => {
+  passport.authenticate("login", async (err, user, info) => {
+    try {
+      if (err || !user) {
+        const error = new Error("An error occurred.");
+
+        return next(error);
+      }
+
+      req.login(user, { session: false }, async (error) => {
+        if (error) return next(error);
+
+        const body = { _id: user._id, email: user.email };
+        const token = JWT.sign(user.toJSON(), process.env.PASSPORT_SECRET);
+
+        res.json({ email: user.email, token: token });
       });
+    } catch (error) {
+      return next(error);
     }
-    req.login(user, { session: false }, (err) => {
-      if (err) res.send(err);
-      const token = JWT.sign(user.toJSON(), process.env.PASSPORT_SECRET);
-      res.json({ email: user.email, token: token });
-    });
   })(req, res, next);
 });
 
-router.post("/register", async (req, res) => {
-  console.log(req.body);
-  try {
-    const user = await db.User.create(req.body);
-    res.json(user);
-  } catch (err) {
-    res.status(400).json({
-      message: "didn't work ",
+router.post(
+  "/signup",
+  passport.authenticate("signup", { session: false }),
+  async (req, res, next) => {
+    res.json({
+      message: "Signup successful",
+      user: req.user,
     });
   }
-});
+);
 
 module.exports = router;
